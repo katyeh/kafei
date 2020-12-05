4f1584541bf29141a61e08d0fa4f08d2daca513e
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-12-04 21:41:35 -0800

    Tag section

--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -7,6 +7,15 @@ const Home = () => {
       <div className="home__contents">
         <div className="home__main">
           <Search />
+          <div className="home__tags">
+            <div className="home__tag tag1">#Art</div>
+            <div className="home__tag tag2">#Blogging</div>
+            <div className="home__tag tag3">#Dance & Theatre</div>
+            <div className="home__tag tag4">#Design</div>
+            <div className="home__tag tag5">#Food & Drink</div>
+            <div className="home__tag tag6">#Lifestyle</div>
+            <div className="home__tag tag7">#Photography</div>
+          </div>
         </div>
       </div>
     </div>
