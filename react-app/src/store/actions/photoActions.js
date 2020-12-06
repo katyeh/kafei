6418094810017cbd30d12b70c6f2c27c1e57e405
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-12-06 15:37:17 -0800

    Stil broken

--- react-app/src/store/actions/photoActions.js
+++ react-app/src/store/actions/photoActions.js
@@ -1,17 +1,15 @@
 import { UPLOAD_PHOTO } from "../reducers/uploadPhotoReducer";
 
 export const uploadPhoto = (id, photo) => {
-  debugger
   return async dispatch => {
     try {
       const res = await fetch(`/api/users/${id}/photos`, {
         method: 'POST',
         body: photo
       });
-      
+
       if (res.ok) {
         const data = await res.json()
-        console.log(data)
         return dispatch({
           type: UPLOAD_PHOTO,
           ...data
