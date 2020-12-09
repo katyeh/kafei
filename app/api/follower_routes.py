3f881e23dff84bb95d2509eea29d10289c6c9585
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-12-09 12:27:24 -0800

    Working on model

--- app/api/follower_routes.py
+++ app/api/follower_routes.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, jsonify, request
 from app.models import db, Follower
 import json
+from sqlalchemy import and_
 
 follower_routes = Blueprint('followers', __name__)
 
-# @follower_routes.route('/followers/<int:follower_id>', methods=["DELETE"])
-# def unfollow(user_id, follower_id):
-#     follower_data = Follower.query.filter(Follower.follower_id == follower_id, Follower.followed_id == user_id)
-#     try:
-#         db.session.delete(follower_data)
-#         db.session.commit()
-#         return jsonify(message=f"Unfollowed user with the id of {user_id}.")
-#     except:
-#         return jsonify(message=f"Error unfollowing user with the id of {user.id}.")
+@follower_routes.route('/<int:follower_id>/followed/<int:followed_id>', methods=["DELETE"])
+def unfollow(follower_id, followed_id):
+    follower_data = Follower.query.filter(and_(Follower.follower_id == follower_id,
+                                               Follower.followed_id == followed_id))
+    try:
+        db.session.delete(follower_data)
+        db.session.commit()
+        return jsonify(message=f"Unfollowed user with the id of {followed_id}.")
+    except Exception as error:
+        return jsonify(error=repr(error))
--- app/models/follower.py
+++ app/models/follower.py
@@ -2,6 +2,7 @@ from .db import db
 from sqlalchemy.types import Integer, String
 from sqlalchemy.schema import Column, ForeignKey
 
+
 class Follower(db.Model):
     __tablename__ = 'followers'
 
@@ -12,20 +13,13 @@ class Follower(db.Model):
     follower = db.relationship('User', foreign_keys=[follower_id])
     following = db.relationship('User', foreign_keys=[followed_id])
 
-    user = db.relationship("User", cascade="all, delete", backref="followers", foreign_keys=[follower_id])
-    followed_users = db.relationship("User", cascade="all, delete", backref="followed_users", foreign_keys=[followed_id])
-
+    user = db.relationship("User", cascade="all, delete",
+                           backref="followers", foreign_keys=[follower_id])
+    followed_users = db.relationship(
+        "User", cascade="all, delete", backref="followed_users", foreign_keys=[followed_id])
 
     def to_dict(self):
         return {
-            # "id": self.id,
-            # "follower_id": self.follower_id,
-            # "followed_id": self.followed_id,
-            # "user": {
-            #     "id": self.user.id,
-            #     "name": self.user.name,
-            #     "username": self.user.username
-            # },
             "follower": self.follower.to_dict(),
         }
 
