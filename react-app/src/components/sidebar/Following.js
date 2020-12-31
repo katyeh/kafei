0938fbdc5cb4d9673bd2afc72891190542de4451
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-12-31 01:06:29 -0800

    Can click on following and followers on sidemenu

---
+++ react-app/src/components/sidebar/Following.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+const Following = ({ user, showMenu }) => {
+  const history = useHistory();
+
+  return (
+    <div>
+      <div className="sidemenu__back-div">
+        <div onClick={() => showMenu()} className="sidemenu__arrowback">
+          <ArrowBackIosIcon style={{fontSize: 20}} />
+        </div>
+        <p onClick={() => showMenu()} className="sidemenu__back">Back to main menu</p>
+      </div>
+      <ul>
+        {user.following.map((following) => {
+          return (
+            <li className="sidemenu__follower" onClick={() => history.push(`/users/${following.id}`)}>
+              <img className="sidemenu__follower-pic" src={following.profile_image_url} ></img>
+              <h3 className="sidemenu__follower-name">{following.name}</h3>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Following;
---
+++ react-app/src/components/sidebar/Menu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Menu = ({ user, showFollowers, showFollowing }) => {
+  const history = useHistory();
+
+  return (
+    <div>
+      <div className="sidemenu__stats">
+        <div className="sidemenu__stats-container">
+          <div className="sidemenu__stats-label">Balance</div>
+          <div className="sidemenu__stat">{user.wallet}</div>
+        </div>
+        <div className="sidemenu__stats-container">
+          <div className="sidemenu__stats-label">Coffees</div>
+          <div className="sidemenu__stat">{user.tips}</div>
+        </div>
+          {user.followers &&
+          <div>
+              <div className="sidemenu__stats-container">
+                <div onClick={() => showFollowers()} className="sidemenu__stats-label sidemenu__follow">Followers</div>
+                <div className="sidemenu__stat">{user.followers.length}</div>
+              </div>
+              <div className="sidemenu__stats-container sidemenu__stats-container-bottom">
+                <div onClick={() => showFollowing()} className="sidemenu__stats-label sidemenu__follow">Following</div>
+                <div className="sidemenu__stat">{user.following.length}</div>
+              </div>
+          </div>
+          }
+      </div>
+    </div>
+  )
+}
+
+export default Menu;
